366df1796f8110b097e0ef2d2e3ca47bf5f5eda7
Denis Fazik <denis.fazik@demi.sk>
2025-08-15 16:08:33 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import axios from 'axios'
+function csrfToken() {
+  axios.get('http://localhost:80/sanctum/csrf-cookie')
+  axios.defaults.withCredentials = true
+  axios.defaults.withXSRFToken = true
+}
+csrfToken()
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import App from './App.vue'
+import router from './router'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+app.use(router)
+
+app.mount('#app')
