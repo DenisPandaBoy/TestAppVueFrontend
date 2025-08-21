dd082c7477320073a5a118f519276502939a4aee
Denis Fazik <denis.fazik@demi.sk>
2025-08-21 09:03:24 +0200

    Remove useless catch of errors

--- src/API/Login.ts
+++ src/API/Login.ts
@@ -5,28 +5,20 @@ import { getCsrfCookie } from '@/API/CsrfCookie.ts'
 export const login = (email: string, password: string) => {
   const loginRequest = useAxios()
 
-  return getCsrfCookie()
-    .then(() => {
-      return loginRequest
-        .request({
-          method: 'POST',
-          url: '/login',
-          data: {
-            email: email,
-            password: password,
-          },
-        })
-        .then((res) => {
-          if (res.status === 200) {
-            router.push('/')
-          }
-        })
-        .catch((err) => {
-          throw err
-        })
-    })
-    .catch((err) => {
-      console.log(err)
-      throw err
-    })
+  return getCsrfCookie().then(() => {
+    return loginRequest
+      .request({
+        method: 'POST',
+        url: '/login',
+        data: {
+          email: email,
+          password: password,
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          router.push('/')
+        }
+      })
+  })
 }
